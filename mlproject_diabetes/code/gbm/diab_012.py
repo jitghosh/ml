c2b87da53475c0f79f9bcaa5a9bcb144dc542b48
jitghosh <pghosh@microsoft.com>
2024-11-13 08:45:26 -0500

    na

--- mlproject_diabetes/code/gbm/diab_012.py
+++ mlproject_diabetes/code/gbm/diab_012.py
@@ -6,14 +6,17 @@ from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score
 from sklearn.feature_selection import GenericUnivariateSelect, chi2
 from sklearn.model_selection import train_test_split, StratifiedKFold, ParameterGrid
 import matplotlib.pyplot as plt
+import os, pathlib, pickle
 
 random_state = 1234
 
+
+
 # %%
-df_diab_012 = pd.read_csv("..\..\data\diabetes_012_health_indicators_BRFSS2015.csv")
-df_diab_012.head()
+df_diab_012 = pd.read_csv("C:/pix/ml/mlproject_diabetes/data/diabetes_012_health_indicators_BRFSS2015.csv")
 # %%
 # feature selection
+# use a chi-squared test to select the best features
 p_value = 0.05
 selector = GenericUnivariateSelect(score_func=chi2, mode="fpr", param=p_value).fit(
     df_diab_012.drop("Diabetes_012", axis=1), df_diab_012["Diabetes_012"]
@@ -39,48 +42,85 @@ cat_features = all_features.difference(set(numeric_features))
 
 
 # %%
-# training parameters
+# constant training parameters
 const_params: dict = {
     "seed": random_state,
+    "verbosity": 0,
     "objective": "multiclass",
     "num_threads": 12,
     "num_classes": 3,
     "is_unbalance": True,
     "metric": "softmax",
+    #"early_stopping_rounds": 5,
+    #"early_stopping_min_delta": 0.0025
 }
+# parameters to search for using grid search
 search_param_grid: ParameterGrid = ParameterGrid(
     {
         "data_sample_strategy": ["goss", "bagging"],
-        "num_rounds": range(100, 500, 100),
+        "num_iterations": range(100, 600, 100),
         "learning_rate": [0.01, 0.001, 0.0001],
         "boosting": ["dart", "gbdt"],
+        #"max_depth": [8,16,24,32,-1],
+        #"min_samples_leaf": [4,8,12,20],
+        #"feature_fraction": [1.0,0.8]
     }
 )
-k_fold_splits = 5
-# %%
 
-param_scores = []
-for params in search_param_grid:
-    print(f"Using {params}")
-    skfold = StratifiedKFold(
-        n_splits=k_fold_splits, shuffle=True, random_state=random_state
-    )
-    validation_scores = []
-    for idx, (train_idx, valid_idx) in enumerate(skfold.split(train_X, train_y)):
-        ds_train_X = lgbm.Dataset(
+# %%
+if not pathlib.Path("./diab_012_bestparam.pkl").is_file():
+    k_fold_splits = 5
+    param_validation_loss = []
+    # hyper parameter search
+    for params in search_param_grid:
+        print(f"Using {params}")
+        skfold = StratifiedKFold(
+            n_splits=k_fold_splits, shuffle=True, random_state=random_state
+        )
+
+        validation_loss = []
+        for idx, (train_idx, valid_idx) in enumerate(skfold.split(train_X, train_y)):
+            fold_validation_loss = {}
+            ds_train_X = lgbm.Dataset(
+                train_X.iloc[train_idx, :],
+                label=train_y.iloc[train_idx],
+                feature_name=all_features,
+                categorical_feature=cat_features,
+            )
+            ds_valid_X = lgbm.Dataset(
+                train_X.iloc[valid_idx, :],
+                label=train_y.iloc[valid_idx],
+                feature_name=all_features,
+                categorical_feature=cat_features,
+            )
+
+            booster = lgbm.train(const_params | params, ds_train_X, valid_sets=[ds_valid_X],valid_names=["validation"], callbacks=[lgbm.record_evaluation(fold_validation_loss)])
+            
+            validation_loss.append(fold_validation_loss["validation"]["multi_logloss"][-1])
+            
+        param_validation_loss.append((np.mean(validation_loss),params))
+        print(f"Validation loss: {param_validation_loss[-1]}")
+
+    param_validation_loss = sorted(param_validation_loss,key=lambda tup:tup[0])
+    best_params = param_validation_loss[0][1]
+    print(f"best_params : {best_params}, log loss : {param_validation_loss[0][0]}")
+#%%
+    with open("./diab_012_bestparam.pkl","wb+") as best_params_file:
+        pickle.dump(best_params,best_params_file)
+#%%
+
+if pathlib.Path("./diab_012_bestparam.pkl").is_file():
+    with open("./diab_012_bestparam.pkl","rb+") as best_params_file:
+        best_params = pickle.load(best_params_file)
+else:
+    raise "Need to rerun hyper parameter search"
+#%%
+
+ds_train_X = lgbm.Dataset(
             train_X.iloc[train_idx, :],
             label=train_y.iloc[train_idx],
             feature_name=all_features,
             categorical_feature=cat_features,
         )
-        booster = lgbm.train(const_params | params, ds_train_X)
-        validation_preds = booster.predict(train_X.iloc[valid_idx, :])
-
-        validation_scores.append(f1_score(
-            np.argmax(validation_preds, axis=1), train_y.iloc[valid_idx], pos_label=range(3), average="weighted"
-        ))
-    param_scores.append((np.mean(validation_scores),params))
-#%%
-best_param = sorted(param_scores,key=lambda tup:tup[0],reverse=True)[0][1]
-print(best_param)
+best_booster = lgbm.train(const_params | best_params, ds_train_X)
 # %%
