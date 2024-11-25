f7bd292b1b382f68935b6b946633763215b1dd13
jitghosh <pghosh@microsoft.com>
2024-11-25 11:11:03 -0500

    na

--- hw/kmeans/kmeans.py
+++ hw/kmeans/kmeans.py
@@ -8,39 +8,83 @@ import matplotlib.pyplot as plt
 
 # %%
 class KMeansLearner:
-    def __init__(self, n_clusters=5,centroids = None):
-        self.n_clusters = n_clusters
+    '''
+    This class has no predict. This is unsupervised learning so there is nothing to predict. We just fit and 
+    find the clusters (i.e. the cluster centroids) 
+    '''
+    def __init__(self, k=5,centroids = None):
+        '''
+        Parameters:
+            k: number of clusters we want to find
+            centroids: Initial value for centroids if supplied. 
+            Will be a k x d array where k is the number of clusters and d is the dimension of the data 
+            (i.e. number of features) i.e. one centroid per cluster
+        '''
+        self.k = k
         self.centroids = centroids
+        # we also store the centroids at each iteration
         self.iteration_history = []
 
     def fit(self, train_X):
+        '''
+        Parameters:
+            train_X: the data we want to cluster. Of shape Nxd.
+        '''
+        # if we were not given initial values of centroids during object ctor, we initialize now
+        # to a k x d array (one centroid per cluster) of random float values
         if(self.centroids is None):
-            self.centroids = np.random.random((self.n_clusters, train_X.shape[1]))
+            self.centroids = np.random.random((self.k, train_X.shape[1]))
+        # this is our very first set of centroids - so add them to the history
         self.iteration_history.append(self.centroids.copy())
         # list of clusters that each training sample belongs to
         cluster_memberships = None
+
+        # we run the loop for as long as needed
         while True:
+            # we calculate distances between each of our training samples and the current centroids
+            # we are going to have k distances for each training sample, one for each centroid. 
+            # So distances is going to be a N x k array
+            # Like before we pass in the second argument to calc_distance as a named argument to
+            # apply_along_axis
             distances = np.apply_along_axis(
                 kml.calc_distance, arr=train_X, axis=1, centroids=self.centroids
             )
+            # for each of the training samples, the cluster it belongs to will be the centroid number
+            # that is has the least distance to. Argmin will give us a (150,) array of centroid number (cluster membership)
+            # for each sample 
             cluster_memberships = np.argmin(distances, axis=1)
 
+            # now we need to recalculate the centrids based on the sample cluster memberships.
+            # To do that we first stack(join) the training data with the cluster membership data 
+            # (effectively stacking a new column at the end of the training data).
             X_with_cluster = np.hstack((train_X, cluster_memberships.reshape(-1, 1)))
-            update_count = 0
-            for cluster_idx in range(self.n_clusters):
+            # indicator variable to control the loop
+            centroids_updated = False
+            # Now for each cluster
+            for cluster_idx in range(self.k):
+                # we find the samples which are now predicted to be the members of this cluster in this iteration
                 X_group_for_cluster = X_with_cluster[
-                    X_with_cluster[:, -1] == cluster_idx, :-1
+                    X_with_cluster[:, -1] == cluster_idx, :-1 # exclude the last cluster membership column
                 ]
+                # if no samples are members of this cluster - continue
                 if X_group_for_cluster.shape[0] == 0:
                     continue
+                # calculate the new centroid of this cluster as a mean of the sample subset belonging to this cluster
                 new_centroid = np.mean(X_group_for_cluster, axis=0)
+                # the new centroid value is the same as the old one - continue
                 if np.all(new_centroid == self.centroids[cluster_idx, :]):
                     continue
                 else:
+                    # update the centroid for this cluster
                     self.centroids[cluster_idx, :] = new_centroid
-                    update_count += 1
+                    # mark the indicator to indicate that in this iteration at least one centroid was updated
+                    centroids_updated = True
+            # make a copy of the centroids and append it to the history. Making a copy is important. 
+            # If you do not then in each loop you will be updating the same centroids.  
             self.iteration_history.append(self.centroids.copy())
-            if update_count == 0:
+
+            # no centroids updated - we are done
+            if not centroids_updated:
                 break
 
     def calc_distance(self, X_from, centroids):
@@ -48,13 +92,14 @@ class KMeansLearner:
 
 
 # %%
+# data processing code and prediction/accuracy code here. Do not forget to scale data since KNN is scale sensitive 
 
 df_iris = pd.read_csv(
     "./iris.data",
     header=None,
     names=["petal_length", "petal_width", "sepal_length", "sepal_width", "cls"],
 )
-kml = KMeansLearner(n_clusters=3)
+kml = KMeansLearner(k=3)
 data = StandardScaler().fit_transform(df_iris.loc[:,["petal_length","petal_width"]])
 kml.fit(data)
 fig,ax = plt.subplots(1,1)
@@ -64,7 +109,7 @@ ax.scatter(kml.centroids[:,0],kml.centroids[:,1],marker='D', c='y')
 
 
 # %%
-kml = KMeansLearner(n_clusters=3, centroids=np.array([[6.6,3.7],[6.2,3.2],[6.5,3.0]]))
+kml = KMeansLearner(k=3, centroids=np.array([[6.6,3.7],[6.2,3.2],[6.5,3.0]]))
 
 data = np.array(
     [
--- hw/knn/yeast_knn.py
+++ hw/knn/yeast_knn.py
@@ -12,32 +12,82 @@ random_state = 1234
 
 
 # %%
+'''
+KNN Classifier class
+'''
 class KNNClassfier:
     def __init__(self, n_neighbors=7):
+        '''
+        Parameters:
+            n_neighbors: How many neighbors should we look at when determining "closeness"
+        '''
+
+        # member variable to store the number of neighbors to look at when predicting 
         self.n_neighbors = n_neighbors
+        # member variable to store the training data features. Will be set in fit()
         self.X = None
+        # member variable to store the training data labels. Will be set in fit() 
         self.y = None
 
     def fit(self, X, y):
+        '''
+        We do nothing in fit() other than simply storing the training data to be used during predict.
+        We traditionally return self (the classifier object itself) from fit() as a pattern so that 
+        we can chain calls i.e. do something like classifier = KNNClassfier(7).fit(X,y) in one step 
+        (which other wise can be done in two steps like classifier = KNNClassifier(7) and then classifier.fit(X,y))
+        This is not required - but scikit learn implements this pattern. 
+        '''
         self.X = X
         self.y = y
         return self
 
     def predict(self, test_X):
+        '''
+        Return the majority class for each sample in test_X
+
+        Parameters:
+            test_X: test data (features only) of shape Txd i.e. T samples with d features each
+        '''
+
+        # apply_along_axis() when called with axis=1 will supply one slice along the columns (i.e. one row) of the arr data 
+        # at a time to the supplied callable (in our case find_majority_class). This is equivalent to looping over the dataset 
+        # rows and calling the function ourselves with one row at a time passed into it, but this is much faster since it is 
+        # vectorized
         return np.apply_along_axis(self.find_majority_class,arr=test_X,axis=1)
 
     def find_majority_class(self, target_x):
+
+        # calculate the distances from each of the training samples to the target test sample 
+        # Once again we use apply_along_axis to iterate over the training samples we have already
+        # stored. The second parameter to calc_distance is passed as a named (X_to) parametet to 
+        # apply_along_axis which in turn will pass it along to calc_distance for each call 
         distances = np.apply_along_axis(
             self.calc_distance, axis=1, arr=self.X, X_to=target_x
         )
+        # The returned distances should be of shape Nx1 where we have N training samples and a distance 
+        # of each training sample to the target test sample. Our goal is to find 'n_neighbors' number of samples
+        # with the lowest distances from the test target sample and then take the majority label for those. 
+        # So we need to sort the distances. We join the distances with the training labels (which is also of the same shape Nx1)
+        # so that when we sort the distances the lables get sorted with it at the same time.
+        
+        # Join into a dataframe supplying the data as a dict (the keys becomes columns in the dataframe)
         y_dist = pd.DataFrame({"dist": distances, "y": self.y})
+        # Sort the dataframe by the dist column in ascending order
         sorted_values = y_dist.sort_values(by=["dist"], ascending=True).values
-        highest_count = Counter(sorted_values[0 : self.n_neighbors, 1]).most_common(1)
+        # take the 'n_neighbors' number of labels from the beginning
+        top_n_neighbors = sorted_values[0 : self.n_neighbors, 1]
+        # Use the Counter class to find the counts per label (returns a dictionary with labels as keys as counts as values)
+        label_frequencies = Counter(top_n_neighbors)
+        # Find the label with highest frequency - check the docs for most_common and Counter
+        highest_count = label_frequencies.most_common(1)
+        # return the label with the highest frequency - will be a list with a single tuple of label and its count 
         return highest_count[0][0]
 
     def calc_distance(self, X_from, X_to):
         return linalg.norm(X_to - X_from)
 
+#%%
+# data processing code and prediction/accuracy code here. Do not forget to scale data since KNN is scale sensitive 
 
 # %%
 col_names = [
